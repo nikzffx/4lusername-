07fa6741bbdd2afa47e42f23a16d0bffd80bfc4b
nikzffx <nikzbahi@gmail.com>
2025-02-17 03:47:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,17 @@ import random
 import string
 import aiohttp
 from pyfiglet import Figlet
-from flask import Flask
 import asyncio
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Instagram bot is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
 
 # Credits to GOD
