84e4a8882a557066677dced03204ab6767e74c97
nikzffx <nikzbahi@gmail.com>
2025-02-17 03:43:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,19 +5,8 @@ import string
 import aiohttp
 from pyfiglet import Figlet
 from flask import Flask
-import threading
 import asyncio
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=8080)
-# Start the Flask server in a separate thread
-threading.Thread(target=run_flask).start()
 
 # Credits to GOD
 print("Credits to @We_areGOD ")
